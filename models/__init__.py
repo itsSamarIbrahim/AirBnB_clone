ab3e751345fcbacabb429a87a93cb07ff391e49b
Samar <bantmasr21@gmail.com>
2024-02-09 23:30:50 +0200

    added

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from AirBnB_clone.models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """
 
 """
+from engine.file_storage import FileStorage
 from uuid import uuid4
 from datetime import datetime
 
@@ -22,12 +23,14 @@ class BaseModel:
             self.id = str(uuid4)
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            FileStorage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] (self.id) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        FileStorage.save()
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()
