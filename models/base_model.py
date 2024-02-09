8f531a9340256a9daeb63d3df40cc01c28ec2e84
itsSamarIbrahim <>
2024-02-09 06:15:37 -0800

    added

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+
+"""
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """
+    """
+    def __init__(self):
+        self.id = str(uuid4)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] (self.id) {self.__dict__}"
+
+    def save(self):
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
+
+
+#if __name__ == "__main__":
+    #base_model = BaseModel()
+    #print(base_model)
+    #print()
+    #base_model.save()
+    #print(base_model)
+    #print()
+    #print(base_model.to_dict())
---
+++ tests/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.base_model BaseModel()
+
+
