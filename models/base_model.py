cfe0063063942434ca085c216f701bc140269ae5
Samar <bantmasr21@gmail.com>
2024-02-09 17:20:09 +0200

    added

--- models/base_model.py
+++ models/base_model.py
@@ -5,19 +5,29 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """
     """
-    def __init__(self):
-        self.id = str(uuid4)
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] (self.id) {self.__dict__}"
 
     def save(self):
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()
@@ -27,11 +37,11 @@ class BaseModel:
         return dict_copy
 
 
-#if __name__ == "__main__":
-    #base_model = BaseModel()
-    #print(base_model)
-    #print()
-    #base_model.save()
-    #print(base_model)
-    #print()
-    #print(base_model.to_dict())
+# if __name__ == "__main__":
+#     base_model = BaseModel()
+#     print(base_model)
+#     print()
+#     base_model.save()
+#     print(base_model)
+#     print()
+#     print(base_model.to_dict())
