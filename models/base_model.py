694e8daf2f03730221148f456eda6cbac418e9dc
itsSamarIbrahim <>
2024-02-11 10:40:22 -0800

    fixed a bug

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
+"""This script is the base model"""
 
 import uuid
 from datetime import datetime
-from models.storage import storage
+from models import storage
 
 
 class BaseModel:
-    """
-    A class defining common attributes/methods for other classes.
-    """
+
+    """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize BaseModel instance.
+        """Initializes instance attributes
 
         Args:
-            *args: Additional positional arguments.
-            **kwargs: Additional keyword arguments.
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
         """
-        if kwargs:
-            for key, value in kwargs.items():
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
                 if key == "created_at":
-                    self.created_at = datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.updated_at = datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, key, value)
-
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
-
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -41,27 +35,22 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """
-        Return string representation of BaseModel instance.
-        """
-        return "[{}] \
-({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Update the public instance attribute updated_at
-        with the current datetime.
-        """
+        """updates the public instance attribute updated_at"""
+
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """
-        Return a dictionary containing all keys/values
-        of __dict__ of the instance.
-        """
-        dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = type(self).__name__
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
-        return dict_copy
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
