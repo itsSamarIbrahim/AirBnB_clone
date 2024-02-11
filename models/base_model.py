65d25124f6dd9057a9c08a5e9ac39125c0bbaad4
itsSamarIbrahim <>
2024-02-11 10:21:14 -0800

    fixed a bug

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """
 A class BaseModel that defines all common attributes/methods for other classes
 """
-from engine.file_storage import FileStorage
 from uuid import uuid4
 from datetime import datetime
 
@@ -12,7 +11,7 @@ class BaseModel:
     BaseModel class defines common attributes/methods for other classes.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, storage=None, *args, **kwargs):
         """
         Initialize BaseModel instance.
         """
@@ -25,11 +24,19 @@ class BaseModel:
                         value, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, value)
+
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+
         else:
             self.id = str(uuid4)
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            FileStorage.new(self)
+            if storage:
+                storage.new(self)
+        self.storage = storage
 
     def __str__(self):
         """
@@ -39,14 +46,17 @@ class BaseModel:
 
     def save(self):
         """
-        Update the public instance attribute updated_at with the current datetime.
+        Update the public instance attribute
+        updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
-        FileStorage.save()
+        if self.storage:
+            self.storage.save()
 
     def to_dict(self):
         """
-        Return a dictionary containing all keys/values of __dict__ of the instance.
+        Return a dictionary containing all keys/values
+        of __dict__ of the instance.
         """
         dict_copy = self.__dict__.copy()
         dict_copy['__class__'] = self.__class__.__name__
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        key = "{obj.__class__.name}.{obj.id}"
+        key = f"{obj.__class__.name}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
@@ -18,19 +18,19 @@ class FileStorage:
         for key, obj in self.__objects.items():
             class_name = obj.__class__.__name__
             obj_id = obj.id
-            serialized_objects[f"{class_name}.{obj_id}"] = obj.to_dict()
+            serialized_objects[obj_id] = obj.to_dict()
         with open(self.__file_path, 'w') as File:
             json.dump(serialized_objects, File)
 
     def reload(self):
         try:
-            with open(self.__file_path, 'r') as File:
+            with open(self.__file_path, 'r', encoding="UTF-8") as File:
                 loaded_objects = json.load(File)
-                for key, obj_dict in loaded_objects.items():
-                    class_name, obj_id = key.split('.')
-                    module = __import__('models.' + class_name.lower(), fromlist=[class_name])
-                    cls = getattr(module, class_name)
+                for obj_id, obj_dict in loaded_objects.items():
+                    class_name = obj_dict['__class__']
+                    cls = globals()[class_name]
                     obj = cls(**obj_dict)
+                    key = f"{class_name}.{obj_id}"
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
